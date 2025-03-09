fc8f44e90019583e6b860d260592b9cfed8752f1
kentonla <116829705+kentonla@users.noreply.github.com>
2025-03-08 23:55:41 -0800

    updated alternate to switch between states

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import time
+import random
+from threading import Thread
+# https://docs.python.org/3/library/threading.html
 
-
-# Create 5 philosophers, identified by a number 0, 1, 2, 3, and 4
-# Each philosopher runs as a seprate thread
-def create_philosphers():
-    pass
 
 # Uses philosopher_number to identify how many philosphers wishing to eat.
 # When a philosopher finishes eating, a call is made to return_forks(philosopher_number)
@@ -13,17 +12,30 @@ def pickup_forks(philosopher_number):
 def return_forks(philosopher_number):
     pass
 
-def philosopher_thinking():
+# Alternates philosopher between thinking and eating
+# Handles all of the actions a philosopher does
+# A philosopher has to think to pickup fork, then eat, and then put down the fork
+def alternate(philosopher_number):
+    philosopher_thinking(philosopher_number)
+    pickup_forks(philosopher_number)
+    philosopher_eating(philosopher_number)
+    return_forks(philosopher_number)
+    
+
+def philosopher_thinking(philosopher_number):
     pass
 
-def philosopher_eating():
+def philosopher_eating(philosopher_number):
     pass
 
 # To stimulate both thinking and eating, this function will put threads to sleep for a random period of time between 1-3 seconds
 def random_thread_sleep():
-    pass
+    delay = random.randint(1,3)
+    time.sleep(delay)
 
 
 if __name__ == "__main__":
+    # Create 5 philosophers, identified by a number 0, 1, 2, 3, and 4
+    # Each philosopher runs as a seprate thread
     
     exit
