e4b02805cf676994911824eab02fb1e24e3cd09c
kentonla <116829705+kentonla@users.noreply.github.com>
2025-03-08 20:49:49 -0800

    setup python file with base functions listed out

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+# Create 5 philosophers, identified by a number 0, 1, 2, 3, and 4
+# Each philosopher runs as a seprate thread
+def create_philosphers():
+    pass
+
+# Uses philosopher_number to identify how many philosphers wishing to eat.
+# When a philosopher finishes eating, a call is made to return_forks(philosopher_number)
+def pickup_forks(philosopher_number):
+    pass
+
+def return_forks(philosopher_number):
+    pass
+
+def philosopher_thinking():
+    pass
+
+def philosopher_eating():
+    pass
+
+# To stimulate both thinking and eating, this function will put threads to sleep for a random period of time between 1-3 seconds
+def random_thread_sleep():
+    pass
+
+
+if __name__ == "__main__":
+    
+    exit
